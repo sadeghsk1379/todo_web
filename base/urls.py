feed753d1faa8e665d6e2d216ec8eb655de936c7
sadeghsk <sadeghsargazi2000@gmail.com>
2024-01-21 06:18:47 +0330

    add create, list , detail class view

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-from .views import TaskList
+from .views import TaskCreate, TaskDetail, TaskList, TaskUpdate
 
-urlpatterns = [path("", TaskList.as_view(), name="tasks")]
+urlpatterns = [
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task/", TaskCreate.as_view(), name="task-create"),
+    path("task-update/<int:pk>/", TaskUpdate.as_view(), name="task-update"),
+]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,6 @@
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 
 from .models import Task
@@ -5,3 +8,22 @@ from .models import Task
 
 class TaskList(ListView):
     model = Task
+    context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "base/task.html"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
