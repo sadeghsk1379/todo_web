edea265f9434c8aba9319c448ca81faac0946de9
Your Name <you@example.com>
2024-01-21 18:56:02 +0330

    add update and delete view

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
 from .models import Task
@@ -26,4 +26,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = "task"
     success_url = reverse_lazy("tasks")
